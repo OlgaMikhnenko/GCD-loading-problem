0dd9cf89faa16c81a9fd507c521bce672127fd59
Olga Mikhnenko <olga230397@icloud.com>
2021-02-15 21:31:35 +0300

    Initial Commit

---
+++ GCD loading problem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD loading problem
+//
+//  Created by Helga on 15.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
